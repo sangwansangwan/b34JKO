32c39d7cf4978f6377354e2204abb6145017006b
sangwanritik <ritik.sangwan@email.com>
2023-11-21 00:33:09 +0530

    fixed cors

--- Backend/index.js
+++ Backend/index.js
@@ -50,7 +50,7 @@ var ownersignup = require("./controllers/ownersignup");*/
 // import { profile } from "./controllers/profile";
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://83.85.157.106:3000", credentials: true }));
 
 //use express session to maintain session data
 app.use(
